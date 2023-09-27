4a248574feb7cf28d5e2697806e2e213566315e0
Jaco Esterhuizen <jaco@litriv.com>
2023-09-27 15:22:29 +0200

    Create BlockConsumer with failing tests
    The BlockConsumer just wraps an iterator, itself implementing Iterator,
    calling through to the wrapped iterator.  This means the tests are
    failing.  Next up is to add the block identification and consumption
    behaviour, instead of just calling through to the wrapped iterator.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,37 @@
+pub struct BlockConsumer<I, CF> {
+    iter: I,
+    cf: CF,
+}
+impl<I, CF> Iterator for BlockConsumer<I, CF>
+where
+    I: Iterator,
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next()
+    }
+}
+
+pub fn block_consumer<I, CF>(iter: I, cf: CF) -> BlockConsumer<I, CF>
+where
+    I: Iterator,
+    CF: FnMut(I::Item) -> I::Item,
+{
+    BlockConsumer { iter, cf }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
     fn it_works() {
-        assert!(true);
+        let v = [0, 0, 1, 2, 3, 0, 4, 5, 0, 0, 0, 6, 7, 8, 0, 0];
+        let mut bc = block_consumer(v.into_iter(), |v| v + 1);
+        assert_eq!(bc.next(), Some(6));
+        assert_eq!(bc.next(), Some(9));
+        assert_eq!(bc.next(), Some(21));
+        assert_eq!(bc.next(), None);
     }
 }
