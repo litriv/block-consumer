9b0f3af8e9890ee86554c000c649ac6677d4916a
Jaco Esterhuizen <jaco@litriv.com>
2023-09-30 18:18:03 +0200

    Move tests to tests/integration_test.rs
    That starts testing from the POV of users of this library.

--- src/lib.rs
+++ src/lib.rs
@@ -59,15 +59,9 @@ where
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
     fn it_works() {
-        let v = [0, 0, 1, 2, 3, 0, 4, 5, 0, 0, 0, 6, 7, 8, 0, 0];
-        let mut bc = BlockConsumer::new(v.into_iter(), |v| *v == 0, |orig, v| v + orig);
-        assert_eq!(bc.next(), Some(6));
-        assert_eq!(bc.next(), Some(9));
-        assert_eq!(bc.next(), Some(21));
-        assert_eq!(bc.next(), None);
+        assert!(true);
     }
 }
