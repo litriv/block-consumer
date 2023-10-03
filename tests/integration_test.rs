88cbe3e0e28e39e3b9f351e788f7d4f01c4fbcff
Jaco Esterhuizen <jaco@litriv.com>
2023-10-03 13:55:26 +0200

    Add a test for testing iterator with borrowed items
    This fails and I don't know how to best solve it, so I'm asking on the
    interwebs.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -9,3 +9,13 @@ fn test_owned() {
     assert_eq!(bc.next(), Some(21));
     assert_eq!(bc.next(), None);
 }
+
+#[test]
+fn test_borrowed() {
+    let v = [0, 0, 1, 2, 3, 0, 4, 5, 0, 0, 0, 6, 7, 8, 0, 0];
+    let mut bc = BlockFolder::new(v.iter(), |v| **v == 0, |orig, v| (**v + **orig));
+    assert_eq!(bc.next(), Some(&6));
+    assert_eq!(bc.next(), Some(&9));
+    assert_eq!(bc.next(), Some(&21));
+    assert_eq!(bc.next(), None);
+}
